ee6137c1e5b1cb2b53cc465eafafe7f423731467
JoaoLuisRicken <joaoluism.ricken@gmail.com>
2025-09-26 20:55:05 -0300

    base

---
+++ lista-8/src/main/java/com/example/lista8/Lista8Application.java
@@ -0,0 +1,13 @@
+package com.example.lista8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista8Application.class, args);
+	}
+
+}
